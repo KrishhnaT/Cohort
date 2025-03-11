4e6284588e239c399967e6fc2bc9e3b7d927b72b
KrishhnaT <tupekrishhna@gmail.com>
2025-03-11 16:39:59 +0530

    Mar 11

---
+++ Mdndocs/ARRAYS/Filter.js
@@ -0,0 +1,17 @@
+const words = ["spray", "elite", "exuberant", "destruction", "present"];
+
+const result = words.filter((word) => word.length > 6);
+
+console.log(result);
+// Expected output: Array ["exuberant", "destruction", "present"]
+
+/* The filter() method of Array instances creates a shallow 
+copy of a portion of a given array, filtered down to just
+ the elements from the given array that pass the test 
+ implemented by the provided function
+ */
+
+
+//  filter(callbackFn)
+//  filter(callbackFn, thisArg)
+ 
