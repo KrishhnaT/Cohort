64423287f5b66fd51ebd1e17b959f5e857ffe4ab
KrishhnaT <tupekrishhna@gmail.com>
2025-04-10 16:44:09 +0530

    Apr 10

---
+++ Practice-JS/01/script.js
@@ -0,0 +1,11 @@
+//element to the page
+
+const body = document.body;
+// body.append("Hello world ")
+
+const div = document.createElement("div")
+// div.innerText = "Heyy div method 1"
+
+// div.textContent = "Hello div method 2"
+
+body.append(div)
