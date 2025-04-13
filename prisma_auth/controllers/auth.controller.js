bb3d7aecc3688f0cbb2e6ef448a57eaa6c5229db
KrishhnaT <tupekrishhna@gmail.com>
2025-04-13 21:52:44 +0530

    Apr 13

--- prisma_auth/controllers/auth.controller.js
+++ prisma_auth/controllers/auth.controller.js
@@ -1,6 +1,13 @@
+import { PrismaClient } from "@prisma/client/extension"
+
+const prisma = new PrismaClient();
+
 const registerUser = async (req,res) => {
      console.log('User Registered')
-}
+     await prisma.user.findUnique({
+          where: {email}
+     })
+};
 
 
 
